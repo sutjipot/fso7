3602232e0c1d8b8384e36294ed1b78edf0f9a9e7
sutjipot <momosutjipto@gmail.com>
2024-06-20 13:48:43 +0300

    7.1

---
+++ routed-anecdotes/src/App.jsx
@@ -0,0 +1,67 @@
+import {
+  BrowserRouter as Router,
+  Routes, Route, Link
+} from 'react-router-dom'
+
+import { useState } from 'react'
+import { About } from './components/About'
+import { AnecdoteList } from './components/AnecdoteList'
+import { Footer } from './components/Footer'
+import { CreateNew } from './components/CreateNew'
+import { Menu } from './components/Menu'
+
+
+const App = () => {
+  const [anecdotes, setAnecdotes] = useState([
+    {
+      content: 'If it hurts, do it more often',
+      author: 'Jez Humble',
+      info: 'https://martinfowler.com/bliki/FrequencyReducesDifficulty.html',
+      votes: 0,
+      id: 1
+    },
+    {
+      content: 'Premature optimization is the root of all evil',
+      author: 'Donald Knuth',
+      info: 'http://wiki.c2.com/?PrematureOptimization',
+      votes: 0,
+      id: 2
+    }
+  ])
+
+  const [notification, setNotification] = useState('')
+
+  const addNew = (anecdote) => {
+    anecdote.id = Math.round(Math.random() * 10000)
+    setAnecdotes(anecdotes.concat(anecdote))
+  }
+
+  const anecdoteById = (id) =>
+    anecdotes.find(a => a.id === id)
+
+  const vote = (id) => {
+    const anecdote = anecdoteById(id)
+
+    const voted = {
+      ...anecdote,
+      votes: anecdote.votes + 1
+    }
+
+    setAnecdotes(anecdotes.map(a => a.id === id ? voted : a))
+  }
+
+  return (
+    <Router>
+      <h1>Software anecdotes</h1>
+      <Menu />
+      <Routes>
+        <Route path="/" element={<AnecdoteList anecdotes={anecdotes} />} />
+        <Route path="/create" element={<CreateNew addNew={addNew} />} />
+        <Route path="/about" element={<About />} />
+      </Routes>
+      <Footer />
+    </Router>
+  )
+}
+
+export default App
---
+++ routed-anecdotes/src/components/AnecdoteList.jsx
@@ -0,0 +1,9 @@
+export const AnecdoteList = ({ anecdotes }) => (
+    <div>
+      <h2>Anecdotes</h2>
+      <ul>
+        {anecdotes.map(anecdote => <li key={anecdote.id} >{anecdote.content}</li>)}
+      </ul>
+    </div>
+  )
+  
